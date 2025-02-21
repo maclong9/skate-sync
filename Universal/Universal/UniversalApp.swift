8ca00e10c7a5ecefde55b6b980d81aeaf8a0e0a6
Mac <maclong9@icloud.com>
2025-02-21 00:21:34 +0000

    chore: formatting

--- Universal/Universal/UniversalApp.swift
+++ Universal/Universal/UniversalApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct UniversalApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
